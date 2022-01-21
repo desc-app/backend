da3b80ea677916086ca9871a56b45c49df83214c
Lillian Armes <lily@hotgirl.zone>
2022-01-20 19:13:16 -0500

    wip: :rocket:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, Responder, get};
+
+// Thought out stack
+// Actix web
+// PostgreSQL using SQLx
+// InfluxDB for metrics and whatnot
+// Redis/EQMX/RabbitMQ if we need network messaging
+// Redis for caching & distributed locks
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
